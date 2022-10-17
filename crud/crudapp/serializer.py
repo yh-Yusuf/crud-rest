7ace3696b16b8630f6a7d9b44b2e08a6e0a76b7b
Yusuf Hasan <62721671+yh-Yusuf@users.noreply.github.com>
2022-10-17 18:34:07 +0530

    Update serializer.py

--- crud/crudapp/serializer.py
+++ crud/crudapp/serializer.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
 from .models import post
+import requests
+
+
 
 class post_serializer(serializers.ModelSerializer):
     class Meta:
         model = post
-        fields = '__all__'
+        fields = ['title', 'content', 'words', 'created_on']
+
+        def validate_title(self, data):
+            if data['title'] == '':
+                raise serializers.ValidationError("Invalid Data")
+
+        def validate_words(self, data):
+            if len(data["words"]) < 3:
+                raise serializers.ValidationError("Length less")
+
+
+            # print(validated_data["words"])
+
+            # words_obj = validated_data['words']
+            # if len(words_obj) < 20:
+            #     raise serializers.ValidationError("lenght less than 20")
+            #
+            # if validated_data.get('title'):
+            #     title_obj = validated_data['title']
+            #     regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+            #
+            #     if not regex.search(title_obj) == None:
+            #         raise serializers.ValidationError("Title contains special characters")
+            #
+            # return validated_data
+
